02775886d453b6e92148a21211fd651ba478e0c5
Dhanunjay Thalluri <157665365+DhanunJay3839@users.noreply.github.com>
2025-05-05 18:58:40 +0530

    updated code for BLOB

--- myFirst/src/main/java/com/example/myFirst/config/Security.java
+++ myFirst/src/main/java/com/example/myFirst/config/Security.java
@@ -28,6 +28,20 @@ public class Security {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(httpBasic -> {});
+        return http.build();
+
+
+
+
+
+    }
+
 
 
 
