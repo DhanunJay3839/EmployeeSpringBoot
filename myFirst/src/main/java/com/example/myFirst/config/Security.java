0c56c68b68294c64cf454f452ba50f1aac48f680
Dhanunjay Thalluri <157665365+DhanunJay3839@users.noreply.github.com>
2025-05-05 14:23:02 +0530

    security updated

--- myFirst/src/main/java/com/example/myFirst/config/Security.java
+++ myFirst/src/main/java/com/example/myFirst/config/Security.java
@@ -2,29 +2,28 @@ package com.example.myFirst.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class Security {
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         return new InMemoryUserDetailsManager(
                 User.withUsername("admin")
-                        .password("admin123")
-                        .roles("USER")
+                        .password(passwordEncoder.encode("admin123"))
+                        .roles("user")
                         .build()
         );
     }
 
     @Bean
-    public static NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
